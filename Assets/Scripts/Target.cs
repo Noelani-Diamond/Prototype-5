f1dfb1f6d47a64f296fc7062a795443d9562a85f
Noelani-Diamond <nceballos26@aoiths.org>
2025-02-07 12:36:03 -0500

    The prefabs are spawning in the scene

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -8,6 +8,8 @@ public class Target : MonoBehaviour
     float minSpeed = 12;
     float maxSpeed = 20;
     float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +17,35 @@ public class Target : MonoBehaviour
 
         targetRb.AddTorque(Random.Range(-maxTorque, maxTorque), Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
         targetRb.AddForce(Vector2.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
 
 
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
